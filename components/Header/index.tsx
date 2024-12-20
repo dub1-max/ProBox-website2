1fa24877b85f5b10e4cb497b48cbbaea243984d8
Yash Avsarmal <134092522+yashavsarmal30@users.noreply.github.com>
2024-12-20 17:15:57 +0530

    theme

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -46,8 +46,12 @@ const Header = () => {
     <header
       className={`fixed left-0 top-0 z-50 w-full py-5 transition-transform duration-200 ease-in-out ${
         stickyMenu ? "bg-transparent shadow-md dark:bg-transparent" : ""
+      }`}
+    >{/**<header
+      className={`fixed left-0 top-0 z-50 w-full py-5 transition-transform duration-200 ease-in-out ${
+        stickyMenu ? "bg-transparent shadow-md dark:bg-transparent" : ""
       } ${hiddenNavbar ? "-translate-y-full" : "translate-y-0"}`}
-    >
+    > for non sticky navbar */}
       <div className="relative mx-auto max-w-c-1390 items-center justify-between px-4 md:px-8 xl:flex 2xl:px-0">
         <div className="flex w-full items-center justify-between xl:w-1/4">
           <a href="/">
--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -15,10 +15,10 @@ const Hero = () => {
         <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
           <div className="flex lg:items-center lg:gap-8 xl:gap-32.5">
             <div className=" md:w-1/2">
-              <h4 className="mb-4.5 text-lg font-medium text-black dark:text-white">
+              <h4 className="mb-4.5 text-lg font-medium text-white dark:text-white">
                 Pro Box
               </h4>
-              <h1 className="mb-5 pr-16 text-3xl font-bold text-black dark:text-white xl:text-hero ">
+              <h1 className="mb-5 pr-16 text-3xl font-bold text-white dark:text-white xl:text-hero ">
               Pro Box {"   "}
                 <span className="relative inline-block before:absolute before:bottom-2.5 before:left-0 before:-z-1 before:h-3 before:w-full before:bg-titlebg dark:before:bg-titlebgdark ">
                    Business centers and many more 
