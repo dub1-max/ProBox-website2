b37371bd862821491336c22b98c1c78e37731016
Yash Avsarmal <134092522+yashavsarmal30@users.noreply.github.com>
2024-12-21 23:15:07 +0530

    map

--- components/static-data/mapConfig.js
+++ components/static-data/mapConfig.js
@@ -1,5 +1,5 @@
 const mapConfig = {
-    googleMapUrl1: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3608.554100745289!2d55.34305767603425!3d25.251939329479967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f5d2b5f33ac7f%3A0xe59c3bea5bf9fa90!2sConnecting%20Minds%20Business%20Center%20LLC!5e0!3m2!1sen!2sin!4v1728818825985!5m2!1sen!2sin",
+    googleMapUrl1: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3608.5375935799143!2d55.334804!3d25.252495!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f5d860e0e9893%3A0x430d10f5f736cfe4!2sBiz%20space%20Business%20Center!5e0!3m2!1sen!2sin!4v1734803055805!5m2!1sen!2sin",
 
 };
 
